055080390d7d9afbeca13fe60fa68f1a2963e66e
Yevhen <zhekan1605@gmail.com>
2026-03-18 09:40:20 +0100

    +logout

--- client/src/components/Profile/Profile.tsx
+++ client/src/components/Profile/Profile.tsx
@@ -1,7 +1,29 @@
+import Header from "../Header/Header"
+import { useNavigate } from "react-router-dom";
+import { IUser } from "../../../../shared/types";
+import { useEffect,useState } from "react";
 export default function Profile(){
+    const [user,setUser]=useState<IUser | null>(null);
+    const navigate=useNavigate();
+    useEffect(()=>{
+        const stored=localStorage.getItem('user');
+        if(!stored) navigate('/login')
+        else setUser(JSON.parse(stored));
+    },[navigate])
+    function handleClick(){
+        localStorage.removeItem('user');
+        localStorage.removeItem('token');
+        navigate('/login');
+    }
+    if(!user) return null;
     return(
         <>
-        <p>Hello world!</p>
+        <div>
+            <h3>{user.username}</h3>
+            <p>{user.height}</p>
+            <p>{user.weight}</p>
+        </div>
+        <button onClick={handleClick}>Log out</button>
         </>
     )
 }
